82509e963541350309e81fb51dd6b28d35b5babf
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2017-09-23 21:24:15 +0700

    Added background job and send notification if there's an update on DTETI's web

--- app/src/main/java/ga/almanalfaruq/tetibot/News.kt
+++ app/src/main/java/ga/almanalfaruq/tetibot/News.kt
@@ -1,8 +1,10 @@
 package ga.almanalfaruq.tetibot
 
+import java.io.Serializable
+
 /**
  * Created by almantera on 19/09/17.
  */
-data class News(var title: String, var category: String, var description: String, var date: String) {
+data class News(var title: String, var category: String, var description: String, var date: String) : Serializable {
     constructor() : this("", "", "", "")
 }
